6870031e19635a13a2bc7b788a23c509fe65031b
Florian Lüke <florianlueke@gmx.net>
2014-09-22 13:53:08 +0200

    add: which (path search) python implementation

--- src/client/mesycontrol/util.py
+++ src/client/mesycontrol/util.py
@@ -5,6 +5,7 @@ from PyQt4.QtCore import QObject, QTimer, pyqtProperty, pyqtSignal
 from PyQt4.QtCore import Qt
 import gc
 import logging
+import os
 import sys
 import weakref
 
@@ -316,3 +317,21 @@ class QtLogEmitter(QObject):
 
     def get_handler(self):
         return self._handler
+
+# source: http://stackoverflow.com/questions/377017/test-if-executable-exists-in-python/377028#377028
+def which(program):
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            path = path.strip('"')
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+
+    return None
