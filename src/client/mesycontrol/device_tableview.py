34b5222d2cbbe8edcc991c63485e195f3f6a9210
Florian Lüke <florianlueke@gmx.net>
2014-10-08 09:16:36 +0200

    Performance improvements in device tabel view.

--- src/client/mesycontrol/device_tableview.py
+++ src/client/mesycontrol/device_tableview.py
@@ -10,7 +10,7 @@ import app_model
 import mrc_command
 import util
 
-column_names  = ('address', 'name', 'value', 'set_value', 'unit_value')
+column_names  = ('address', 'name', 'value',     'set_value',     'unit_value')
 column_titles = ('Address', 'Name', 'Raw Value', 'Set Raw Value', 'Unit Value')
 
 def column_index(col_name):
@@ -25,168 +25,58 @@ def column_name(col_idx):
     except IndexError:
         return None
 
-class ParameterNode(util.TreeNode):
-    data_changed = pyqtSignal()
-
-    def __init__(self, device, address, parent):
-        super(ParameterNode, self).__init__(device, parent)
-        self.address = address
-        self._read_request_id = None
-
-        device.parameter_changed.connect(self._on_parameter_changed)
-        device.config_parameter_value_changed.connect(self._on_config_parameter_changed)
-
-    def _on_parameter_changed(self, address, old, new):
-        if self.address == address:
-            self.data_changed.emit()
-
-    def _on_config_parameter_changed(self, address, value):
-        if self.address == address:
-            self.data_changed.emit()
-
-    def flags(self, column):
-        ret = (Qt.ItemIsSelectable | Qt.ItemIsEnabled)
-
-        if column_names[column] in ('set_value',):
-            ret |= Qt.ItemIsEditable
-
-        return ret
-
-    def get_value(self):
-        if not self.ref.has_parameter(self.address):
-            if self._read_request_id is None and self.ref.is_connected():
-                self._read_request_id = self.ref.read_parameter(self.address, self._handle_read_response)
-            return None
-        return self.ref.get_parameter(self.address)
-
-    def _handle_read_response(self, request, response):
-        self._read_request_id = None
-
-    def get_config(self):
-        if self.ref.config is not None and self.ref.config.contains_parameter(self.address):
-            return self.ref.config.get_parameter(self.address)
-        return None
-
-    def get_profile(self):
-        if self.ref.profile is not None:
-            return self.ref.profile.get_parameter_by_address(self.address)
-        return None
-
-    def data(self, column, role):
-        param_config  = self.get_config()
-        param_profile = self.get_profile()
-        column_name   = column_names[column]
-
-        if (role == Qt.BackgroundRole
-                and column_name in ('value', 'set_value')
-                and self.ref.has_parameter(self.address)
-                and self.get_config() is not None
-                and self.get_value() != self.get_config().value):
-            return QtGui.QColor("#ff0000")
-
-        if role in (Qt.DisplayRole, Qt.EditRole):
-            if column_name == 'address':
-                return self.address
-            elif column_name == 'value':
-                return self.get_value()
-            elif column_name == 'name':
-                if role == Qt.DisplayRole:
-                    return param_profile.name if param_profile is not None else None
-                return param_profile.name if param_profile is not None else str()
-            elif column_name == 'alias':
-                if role == Qt.DisplayRole:
-                    return param_config.alias if param_config is not None else None
-                return param_config.alias if param_config is not None else str()
-            elif column_name == 'set_value':
-                if param_config is not None:
-                    return param_config.value
-                elif self.get_value() is not None:
-                    return self.get_value()
-                elif role == Qt.EditRole:
-                    return int()
-            elif param_profile is not None and len(param_profile.units) > 1 and column_name == 'unit_value':
-                unit = param_profile.units[1] # skip the 'raw' unit
-                raw_value = self.get_value()
-                if raw_value is None:
-                    return None
-                unit_value = unit.unit_value(raw_value)
-                return QtCore.QString.fromUtf8("%f %s" % (unit_value, unit.label))
-
-        return None
-            
-
-    def set_data(self, column, value, role):
-        if role != Qt.EditRole:
-            return False
-
-        column_name  = column_names[column]
-
-        if column_name == 'set_value':
-            int_value, ok = value.toInt()
-            if not ok:
-                return False
-            mrc_command.SetParameter(self.ref, self.address, int_value).start()
-            param_config = self.get_config()
-            if param_config is not None:
-                param_config.value = int_value
-            return True
-        return False
-
-    def context_menu(self, column):
-        ret = QtGui.QMenu()
-        ret.addAction("Refresh").triggered.connect(self._slt_refresh)
-        ret.addAction("Refresh All").triggered.connect(self._slt_refresh_all)
-        return ret
-
-    def _slt_refresh(self):
-        mrc_command.ReadParameter(self.ref, self.address).start()
-
-    def _slt_refresh_all(self):
-        mrc_command.RefreshMemory(self.ref).start()
-
 class DeviceTableModel(QtCore.QAbstractTableModel):
     def __init__(self, device, parent=None):
         super(DeviceTableModel, self).__init__(parent)
-        self.root   = util.TreeNode(None)
+        self.log    = util.make_logging_source_adapter(__name__, self)
+        self._pending_parameters = set()
         self.device = device
 
     def set_device(self, device):
         self.beginResetModel()
-
-        for parameter_node in self.root.children:
-            parameter_node.setParent(None)
-
-        self.root.children = list()
-
-        for i in range(256):
-            parameter_node = ParameterNode(device, i, self.root)
-            parameter_node.data_changed.connect(self._on_parameter_node_changed)
-            self.root.children.append(parameter_node)
-
         self._device = weakref.ref(device)
-
         device.memory_reset.connect(self._reset_model)
-
+        device.add_default_parameter_subscription(self)
+        device.parameter_changed.connect(self._on_device_parameter_changed)
+        device.config_parameter_value_changed.connect(self._on_device_config_parameter_changed)
         self.endResetModel()
 
     def get_device(self):
         return self._device()
 
-    def get_node(self, row):
-        return self.root.children[row]
+    def _on_device_parameter_changed(self, address, old_value, value):
+        self.log.debug("device param changed %d: %d -> %d", address, old_value, value)
 
-    device = pyqtProperty(app_model.Device, get_device, set_device)
+        self.dataChanged.emit(
+                self.createIndex(address, 0),
+                self.createIndex(address, self.columnCount()))
 
-    def _on_parameter_node_changed(self):
-        address = self.sender().address
-        idx1 = self.createIndex(address, 0, self.root)
-        idx2 = self.createIndex(address, self.columnCount(), self.root)
-        self.dataChanged.emit(idx1, idx2)
+    def _on_device_config_parameter_changed(self, address, value):
+        self.log.debug("device config param changed %d = %d", address, value)
+
+        self.dataChanged.emit(
+                self.createIndex(address, 0),
+                self.createIndex(address, self.columnCount()))
 
     def _reset_model(self):
         self.beginResetModel()
         self.endResetModel()
 
+    def get_value(self, address):
+        if not self.device.has_parameter(address):
+            if self.device.is_connected() and address not in self._pending_parameters:
+                self.log.debug("fetching param %d", address)
+                self.device.read_parameter(address, self._handle_read_response)
+                self._pending_parameters.add(address)
+            return None
+        return self.device.get_parameter(address)
+
+    def _handle_read_response(self, request, response):
+        if not response.is_error():
+            self.log.debug("got param %d", response.par)
+            self._pending_parameters.remove(response.par)
+
+    device = pyqtProperty(app_model.Device, get_device, set_device)
 
     # ===== QAbstractItemModel implementation =====
     def columnCount(self, parent=QModelIndex()):
@@ -196,7 +86,7 @@ class DeviceTableModel(QtCore.QAbstractTableModel):
 
     def rowCount(self, parent=QModelIndex()):
         if not parent.isValid():
-            return len(self.root.children)
+            return 256
         return 0
 
     def headerData(self, section, orientation, role=Qt.DisplayRole):
@@ -210,31 +100,86 @@ class DeviceTableModel(QtCore.QAbstractTableModel):
         return None
 
     def flags(self, idx):
-        ret = None
         if idx.isValid():
-            try:
-                ret = self.root.children[idx.row()].flags(idx.column())
-            except NotImplementedError:
-                pass
-        return ret if ret is not None else super(DeviceTableModel, self).flags(idx)
+            ret = (Qt.ItemIsSelectable | Qt.ItemIsEnabled)
+
+            if column_names[idx.column()] in ('set_value',):
+                ret |= Qt.ItemIsEditable
+
+            return ret
+
+        return super(DeviceTableModel, self).flags(idx)
+
 
     def data(self, idx, role=Qt.DisplayRole):
         if not idx.isValid():
             return None
-        return self.root.children[idx.row()].data(idx.column(), role)
+
+        row             = idx.row()
+        col             = idx.column()
+        column_name     = column_names[col]
+        param_config    = self.device.config.get_parameter(row) if self.device.config.contains_parameter(row) else None
+        param_profile   = self.device.profile.get_parameter_by_address(row)
+
+        if (role == Qt.BackgroundRole
+                and column_name in ('value', 'set_value')
+                and self.device.has_parameter(row)
+                and param_config is not None
+                and self.get_value(row) != param_config.value):
+            return QtGui.QColor("#ff0000")
+
+        if role in (Qt.DisplayRole, Qt.EditRole):
+            if column_name == 'address':
+                return row
+            elif column_name == 'value':
+                return self.get_value(row)
+            elif column_name == 'name':
+                if role == Qt.DisplayRole:
+                    return param_profile.name if param_profile is not None else None
+                return param_profile.name if param_profile is not None else str()
+            elif column_name == 'alias':
+                if role == Qt.DisplayRole:
+                    return param_config.alias if param_config is not None else None
+                return param_config.alias if param_config is not None else str()
+            elif column_name == 'set_value':
+                if param_config is not None:
+                    return param_config.value
+                elif self.get_value(row) is not None:
+                    return self.get_value(row)
+                elif role == Qt.EditRole:
+                    return int()
+            elif param_profile is not None and len(param_profile.units) > 1 and column_name == 'unit_value':
+                unit = param_profile.units[1] # skip the 'raw' unit
+                raw_value = self.get_value(row)
+                if raw_value is None:
+                    return None
+                unit_value = unit.unit_value(raw_value)
+                return QtCore.QString.fromUtf8("%f %s" % (unit_value, unit.label))
+
+        return None
 
     def setData(self, idx, value, role = Qt.EditRole):
-        ret = False
-        if idx.isValid():
-            try:
-                ret = self.root.children[idx.row()].set_data(idx.column(), value, role)
-            except NotImplementedError:
-                pass
-        if ret:
+        if role != Qt.EditRole:
+            return False
+
+        row             = idx.row()
+        col             = idx.column()
+        column_name     = column_names[col]
+
+        if column_name == 'set_value':
+            int_value, ok = value.toInt()
+            if not ok:
+                return False
+
+            self.device.set_parameter(row, int_value)
+
             self.dataChanged.emit(
-                    self.index(idx.row(), 0),
-                    self.index(idx.row(), self.columnCount()))
-        return ret
+                    self.createIndex(row, 0),
+                    self.createIndex(row, self.columnCount()))
+
+            return True
+
+        return False
 
 class DeviceTableItemDelegate(QtGui.QStyledItemDelegate):
     def __init__(self, parent=None):
@@ -278,6 +223,13 @@ class DeviceTableSortFilterProxyModel(QtGui.QSortFilterProxyModel):
     def get_filter_read_only(self):
         return self._filter_read_only
 
+    def filterAcceptsColumn(self, src_column, src_parent):
+        # XXX: leftoff
+        return super(DeviceTableSortFilterProxyModel, self).filterAcceptsColumn(src_column, src_parent)
+
+    def filterAcceptsRow(self, src_row, src_parent):
+        return super(DeviceTableSortFilterProxyModel, self).filterAcceptsRow(src_row, src_parent)
+
 class DeviceTableView(QtGui.QTableView):
     def __init__(self, model, parent=None):
         super(DeviceTableView, self).__init__(parent)
@@ -311,10 +263,10 @@ class DeviceTableView(QtGui.QTableView):
     def _slt_context_menu_requested(self, pos):
         idx = self.sort_model.mapToSource(self.indexAt(pos))
 
-        if idx.isValid():
-            node = self.table_model.get_node(idx.row())
-            print node
-            menu = node.context_menu(idx.column())
-            print menu
-            if menu is not None:
-                menu.exec_(self.mapToGlobal(pos))
+        #if idx.isValid():
+        #    node = self.table_model.get_node(idx.row())
+        #    print node
+        #    menu = node.context_menu(idx.column())
+        #    print menu
+        #    if menu is not None:
+        #        menu.exec_(self.mapToGlobal(pos))
