20de6f3ac94e130692748ba886a1476aff230c79
Florian Lüke <florianlueke@gmx.net>
2015-05-05 11:47:26 +0200

    add working MRCComm implementation (serial-tester-boost-asio)

--- serial-tester-boost-asio/src/main.cc
+++ serial-tester-boost-asio/src/main.cc
@@ -1,8 +1,11 @@
+#include <boost/algorithm/string/predicate.hpp>
+#include <boost/assign.hpp>
 #include <boost/asio.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <boost/exception/all.hpp>
 #include <boost/function.hpp>
 #include <boost/make_shared.hpp>
+#include <boost/random.hpp>
 #include <boost/regex.hpp>
 #include <boost/thread/thread.hpp> 
 #include <exception>
@@ -17,32 +20,18 @@ using std::string;
 using std::stringstream;
 namespace asio = boost::asio;
 
-typedef asio::serial_port sp;
-
-typedef boost::function<void (boost::system::error_code, std::size_t)> WriteHandler;
-typedef boost::function<void (boost::system::error_code, std::string)> ReadHandler;
-
-class Serial:
-  public boost::enable_shared_from_this<Serial>,
+class MRCComm:
+  public boost::enable_shared_from_this<MRCComm>,
   private boost::noncopyable
 {
   public:
-    Serial(sp &port)
-      : m_port(port)
-      , m_write_timeout_ms(boost::posix_time::milliseconds(100))
-      , m_read_timeout_ms(boost::posix_time::milliseconds(100))
-      , m_busy(false)
-      , m_timer(m_port.get_io_service())
-    {}
+    typedef boost::function<void (boost::system::error_code, std::size_t)> WriteHandler;
+    typedef boost::function<void (boost::system::error_code, std::string)> ReadHandler;
 
     void write(const std::string &data, WriteHandler handler)
     {
-      // Write one char from the given data string at a time using a timeout of
-      // m_write_timeout_ms.
-      // If all characters have been written or a timeout occured call the given WriteHandler
-      
       if (m_busy)
-        throw std::runtime_error("Serial port is busy");
+        throw std::runtime_error("MRCComm is busy");
 
       m_busy = true;
       m_buf  = data;
@@ -52,12 +41,8 @@ class Serial:
 
     void read(ReadHandler handler)
     {
-      // Read until a timeout or an error occurs.
-      // Call the given ReadHandler with an appropriate error code and the read
-      // data.
-
       if (m_busy)
-        throw std::runtime_error("Serial port is busy");
+        throw std::runtime_error("MRCComm is busy");
 
       m_busy = true;
       m_buf.clear();
@@ -65,24 +50,44 @@ class Serial:
       start_read();
     }
 
+    virtual ~MRCComm() {}
+
+  protected:
+    MRCComm(boost::asio::io_service &io,
+        const boost::posix_time::time_duration &write_timeout = boost::posix_time::milliseconds(100),
+        const boost::posix_time::time_duration &read_timeout  = boost::posix_time::milliseconds(100))
+      : m_io(io)
+      , m_write_timeout(write_timeout)
+      , m_read_timeout(read_timeout)
+      , m_busy(false)
+      , m_timer(io)
+    {}
+
+    virtual void start_write_one(const std::string::const_iterator &it,
+        boost::function<void (boost::system::error_code, std::size_t)> write_handler) = 0;
+
+    virtual void start_read_one(char &dest,
+        boost::function<void (boost::system::error_code, std::size_t)> read_handler) = 0;
+
+    virtual void cancel_io() = 0;
+
   private:
     void handle_timeout(const boost::system::error_code &ec)
     {
       if (ec != asio::error::operation_aborted &&
           m_timer.expires_at() <= asio::deadline_timer::traits_type::now()) {
-        m_port.cancel();
+        cancel_io();
       }
     }
 
-    void start_write(std::string::const_iterator it)
+    void start_write(const std::string::const_iterator &it)
     {
       if (it == m_buf.end()) {
         finish_write(it, boost::system::error_code());
       } else {
-        m_timer.expires_from_now(m_write_timeout_ms);
-        m_timer.async_wait(boost::bind(&Serial::handle_timeout, shared_from_this(), _1));
-        asio::async_write(m_port, asio::buffer(&(*it), sizeof(*it)),
-            boost::bind(&Serial::handle_write, shared_from_this(), it, _1, _2));
+        m_timer.expires_from_now(m_write_timeout);
+        m_timer.async_wait(boost::bind(&MRCComm::handle_timeout, shared_from_this(), _1));
+        start_write_one(it, boost::bind(&MRCComm::handle_write, shared_from_this(), it, _1, _2));
       }
     }
 
@@ -99,7 +104,7 @@ class Serial:
     void finish_write(std::string::const_iterator it, const boost::system::error_code &ec)
     {
       m_timer.cancel();
-      m_port.get_io_service().post(boost::bind(m_write_handler, ec, it - m_buf.begin()));
+      m_io.post(boost::bind(m_write_handler, ec, it - m_buf.begin()));
       m_busy = false;
       m_write_handler = 0;
       m_buf.clear();
@@ -107,10 +112,9 @@ class Serial:
 
     void start_read()
     {
-      m_timer.expires_from_now(m_read_timeout_ms);
-      m_timer.async_wait(boost::bind(&Serial::handle_timeout, shared_from_this(), _1));
-      asio::async_read(m_port, asio::buffer(&m_char_buf, sizeof(m_char_buf)),
-          boost::bind(&Serial::handle_read, shared_from_this(), _1, _2));
+      m_timer.expires_from_now(m_read_timeout);
+      m_timer.async_wait(boost::bind(&MRCComm::handle_timeout, shared_from_this(), _1));
+      start_read_one(m_char_buf, boost::bind(&MRCComm::handle_read, shared_from_this(), _1, _2));
     }
 
     void handle_read(const boost::system::error_code &ec, std::size_t sz)
@@ -127,15 +131,17 @@ class Serial:
     void finish_read(const boost::system::error_code &ec)
     {
       m_timer.cancel();
-      m_port.get_io_service().post(boost::bind(m_read_handler, ec, m_buf));
+      m_io.post(boost::bind(m_read_handler,
+            ec == boost::system::errc::operation_canceled ? boost::system::error_code() : ec,
+            m_buf));
       m_busy = false;
       m_read_handler = 0;
       m_buf.clear();
     }
 
-    sp &m_port;
-    boost::posix_time::time_duration m_write_timeout_ms;
-    boost::posix_time::time_duration m_read_timeout_ms;
+    asio::io_service &m_io;
+    boost::posix_time::time_duration m_write_timeout;
+    boost::posix_time::time_duration m_read_timeout;
     bool m_busy;
     asio::deadline_timer m_timer;
     std::string m_buf;
@@ -144,29 +150,127 @@ class Serial:
     ReadHandler  m_read_handler;
 };
 
-void handle_read(Serial *serial, boost::system::error_code ec, std::string data)
+class MRCSerialComm: public MRCComm
 {
-  cout << "handle_read: " << data << "(" << data.size() << ")" << endl;
+  public:
+    MRCSerialComm(asio::serial_port &port)
+      : MRCComm(port.get_io_service())
+      , m_port(port)
+    {}
+
+  protected:
+    virtual void start_write_one(const std::string::const_iterator &it,
+        boost::function<void (boost::system::error_code, std::size_t)> write_handler)
+    {
+      asio::async_write(m_port, asio::buffer(&(*it), sizeof(*it)), write_handler);
+    }
+
+    virtual void start_read_one(char &dest,
+        boost::function<void (boost::system::error_code, std::size_t)> read_handler)
+    {
+      asio::async_read(m_port, asio::buffer(&dest, sizeof(dest)), read_handler);
+    }
+
+    virtual void cancel_io()
+    {
+      m_port.cancel();
+    }
+
+  private:
+    asio::serial_port &m_port;
+};
+
+class MRCTCPComm: public MRCComm
+{
+  public:
+    MRCTCPComm(asio::ip::tcp::socket &socket)
+      : MRCComm(socket.get_io_service())
+      , m_socket(socket)
+    {}
+
+  protected:
+    virtual void start_write_one(const std::string::const_iterator &it,
+        boost::function<void (boost::system::error_code, std::size_t)> write_handler)
+    {
+      asio::async_write(m_socket, asio::buffer(&(*it), sizeof(*it)), write_handler);
+    }
+
+    virtual void start_read_one(char &dest,
+        boost::function<void (boost::system::error_code, std::size_t)> read_handler)
+    {
+      asio::async_read(m_socket, asio::buffer(&dest, sizeof(dest)), read_handler);
+    }
+
+    virtual void cancel_io()
+    {
+      m_socket.cancel();
+    }
+
+  private:
+    asio::ip::tcp::socket &m_socket;
+};
+
+void handle_write(const boost::shared_ptr<MRCComm> &mrc, boost::system::error_code ec, std::size_t sz);
+
+void handle_read(const boost::shared_ptr<MRCComm> &mrc, boost::system::error_code ec, std::string data)
+{
+  cout << "handle_read: " << data << " (" << data.size() << ")"
+    << "(" << ec << ", " << ec.message() << ")" << endl;
+
+  if (ec) {
+    throw std::runtime_error("read failed");
+  }
+
+  if (data.size() && !boost::algorithm::ends_with(data, "mrc-1>")) {
+    throw std::runtime_error("Could not find prompt in MRC response");
+  }
+
+  static std::vector<std::string> commands = boost::assign::list_of
+    ("?")("SC 0")("SC 1")("LI")("PS")("garbage")("X0")("X1")("RE 0 1 0");
+
+  static boost::random::mt19937 gen;
+  static boost::random::uniform_int_distribution<> dist(0, commands.size()-1);
+
+  const std::string &command(commands[dist(gen)]);
+
+  cout << "Writing '" << command << "'" << endl;
+
+  mrc->write(command + '\r', boost::bind(handle_write, mrc, _1, _2));
 }
 
-void handle_write(boost::system::error_code ec, std::size_t sz)
+void handle_write(const boost::shared_ptr<MRCComm> &mrc, boost::system::error_code ec, std::size_t sz)
 {
-  cout << "handle_write" << ec << sz << endl;
-}
+  cout << "handle_write: " << ec << ", " << ec.message() << " (" << sz << ")" << endl;
 
+  if (ec) {
+    throw std::runtime_error("write failed");
+  }
+
+  mrc->read(boost::bind(handle_read, mrc, _1, _2));
+}
 
 int main(int argc, char *argv[])
 {
   asio::io_service io;
-  sp port(io);
+
+#if 0
+  asio::serial_port port(io);
   port.open("/dev/ttyUSB0");
-  port.set_option(sp::baud_rate(115200));
+  port.set_option(asio::serial_port::baud_rate(115200));
+  boost::shared_ptr<MRCComm> mrc(boost::make_shared<MRCSerialComm>(port));
+#else
+  asio::ip::tcp::socket socket(io);
+  asio::ip::tcp::resolver resolver(io);
+  asio::ip::tcp::resolver::query query("localhost", "4001");
+  asio::ip::tcp::resolver::iterator endpoint_iter(resolver.resolve(query));
+  asio::connect(socket, endpoint_iter);
+  socket.set_option(asio::ip::tcp::no_delay(true));
+  boost::shared_ptr<MRCComm> mrc(boost::make_shared<MRCTCPComm>(socket));
+#endif
 
-  boost::shared_ptr<Serial> serial(boost::make_shared<Serial>(port));
+  boost::function<void (boost::system::error_code, std::string)> read_handler(
+      boost::bind(handle_read, mrc, _1, _2));
 
-  boost::function<void (Serial *, boost::system::error_code, std::string)> read_handler(
-      boost::bind(handle_read, serial.get(), _2, _3));
-
-  io.post(boost::bind(&Serial::read, serial, read_handler));
+  io.post(boost::bind(&MRCComm::read, mrc, read_handler));
   io.run();
 }
