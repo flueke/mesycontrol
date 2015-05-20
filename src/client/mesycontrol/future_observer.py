16910a53c682bff18f4060cec1a0f226817bbe78
Florian Lüke <florianlueke@gmx.net>
2015-05-20 15:40:04 +0200

    FutureObserver: now able to set the future after observer creation

--- src/client/mesycontrol/future_observer.py
+++ src/client/mesycontrol/future_observer.py
@@ -11,15 +11,26 @@ class FutureObserver(QtCore.QObject):
     progress_changed        = pyqtSignal(int)
     progress_text_changed   = pyqtSignal(str)
 
-    def __init__(self, the_future, parent=None):
+    def __init__(self, the_future=None, parent=None):
         super(FutureObserver, self).__init__(parent)
-        self.future             = the_future
-        self._progress_range    = self.future.progress_range()
-        self._progress          = self.future.progress()
-        self._progress_text     = self.future.progress_text()
+        self.future = the_future
 
-        self.future.add_done_callback(self._future_done)
-        self.future.add_progress_callback(self._future_progress)
+    def get_future(self):
+        return self._future
+
+    def set_future(self, the_future):
+        self._future = the_future
+
+        if self.future is not None:
+            self._progress_range    = self.future.progress_range()
+            self._progress          = self.future.progress()
+            self._progress_text     = self.future.progress_text()
+            self.future.add_done_callback(self._future_done)
+            self.future.add_progress_callback(self._future_progress)
+        else:
+            self._progress_range    = (0, 0)
+            self._progress          = 0
+            self._progress_text     = str()
 
     def _future_done(self, f):
         self.done.emit()
