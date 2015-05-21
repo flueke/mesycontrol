f7bc61c36a74a3dc1fa955dabd9df78d883f001d
Florian Lüke <florianlueke@gmx.net>
2015-05-21 16:35:23 +0200

    deleted obsolete files

--- src/client/mesycontrol/hw_tree_view.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# Author: Florian Lüke <florianlueke@gmx.net>
-
-from qt import QtGui
-
-class HardwareTreeView(QtGui.QTreeView):
-    def __init__(self, model, parent=None):
-        super(HardwareTreeView, self).__init__(parent)
