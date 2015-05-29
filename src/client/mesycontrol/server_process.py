fe5ebf35a675e2eb6b2740cf5e24b503ebd12d46
Florian Lüke <florianlueke@gmx.net>
2015-05-29 15:21:50 +0200

    ServerProcess: wait after server startup to observe immediate process exit

--- src/client/mesycontrol/server_process.py
+++ src/client/mesycontrol/server_process.py
@@ -21,8 +21,18 @@ class ServerIsStopped(ServerError):
 
 QProcess = QtCore.QProcess
 
+BASE_PORT = 23000
+
+# Local listen ports and server startup:
+# TODO: move the code that restart the server process with a different local
+# port into ServerProcess itself and make it transparent for the user of the
+# class. Meaning the classes user should not see started(), stopped(), error()
+# or finished() signals while the ServerProcess is still trying to find a
+# working listen port.
+
 class ServerProcess(QtCore.QObject):
     started  = pyqtSignal()
+    stopped  = pyqtSignal()
     error    = pyqtSignal(QProcess.ProcessError, str, int, str)
     finished = pyqtSignal(QProcess.ExitStatus, int, str)
     output   = pyqtSignal(str)
@@ -37,7 +47,9 @@ class ServerProcess(QtCore.QObject):
             127: "exit_unknown_error"
             }
 
-    def __init__(self, binary='mesycontrol_server', listen_address='127.0.0.1', listen_port=23000,
+    startup_delay_ms = 200
+
+    def __init__(self, binary='mesycontrol_server', listen_address='127.0.0.1', listen_port=BASE_PORT,
             serial_port=None, baud_rate=0, tcp_host=None, tcp_port=4001, verbosity=3, parent=None):
 
         super(ServerProcess, self).__init__(parent)
@@ -53,24 +65,34 @@ class ServerProcess(QtCore.QObject):
         self.verbosity = verbosity
 
         self.process = QProcess()
-        self.process.setProcessChannelMode(QtCore.QProcess.MergedChannels)
-        self.process.started.connect(self.started)
+        self.process.setProcessChannelMode(QProcess.MergedChannels)
+
         self.process.error.connect(self._error)
         self.process.finished.connect(self._finished)
         self.process.readyReadStandardOutput.connect(self._output)
 
+        self._startup_delay_timer = QtCore.QTimer()
+        self._startup_delay_timer.setSingleShot(True)
+        self._startup_delay_timer.setInterval(ServerProcess.startup_delay_ms)
+
     def start(self):
+        # Startup procedure:
+        # - start the server process and wait for it to emit started() or error()
+        # - on error: set result to ServerError
+        # - on started: start a timer waiting for startup_delay_ms
+        # - on timeout: if the process is still running: set result to True
+        #               else set result to ServerError
         ret = Future()
 
         try:
-            if self.process.state() != QtCore.QProcess.NotRunning:
+            if self.process.state() != QProcess.NotRunning:
                 raise ServerIsRunning()
 
             args = self._prepare_args()
             program = util.which(self.binary)
 
             if program is None:
-                raise RuntimeError("Could not find server binary '%s'" % self.binary)
+                raise ServerError("Could not find server binary '%s'" % self.binary)
 
             cmd_line = "%s %s" % (program, " ".join(args))
 
@@ -81,12 +103,24 @@ class ServerProcess(QtCore.QObject):
             def on_started():
                 self.log.debug("[pid=%s] Started %s", self.process.pid(), cmd_line)
                 dc()
-                ret.set_result(True)
+                self._startup_delay_timer.timeout.connect(on_startup_delay_expired)
+                self._startup_delay_timer.start()
 
             def on_error(error):
                 self.log.debug("Error starting %s: %d", error)
                 dc()
                 ret.set_exception(ServerError(error))
+
+            def on_startup_delay_expired():
+                self._startup_delay_timer.timeout.disconnect(on_startup_delay_expired)
+                if self.process.state() == QProcess.Running:
+                    self.started.emit()
+                    ret.set_result(True)
+                else:
+                    if self.process.error() != QProcess.UnknownError:
+                        ret.set_exception(ServerError(self.process.errorString()))
+                    else:
+                        ret.set_exception(ServerError(self.exit_code()))
 
             self.process.started.connect(on_started)
             self.process.error.connect(on_error)
@@ -103,7 +137,7 @@ class ServerProcess(QtCore.QObject):
     def stop(self, kill=False):
         ret = Future()
 
-        if self.process.state() != QtCore.QProcess.NotRunning:
+        if self.process.state() != QProcess.NotRunning:
             def on_finished(code, status):
                 self.log.debug("Process finished with code=%d (%s)", code, ServerProcess.exit_code_string(code))
                 self.process.finished.disconnect(on_finished)
@@ -122,10 +156,10 @@ class ServerProcess(QtCore.QObject):
         return self.stop(True)
 
     def is_starting(self):
-        return self.process.state() == QtCore.QProcess.Starting
+        return self.process.state() == QProcess.Starting
 
     def is_running(self):
-        return self.process.state() == QtCore.QProcess.Running
+        return self.process.state() == QProcess.Running
 
     def exit_code(self):
         code = self.process.exitCode()
@@ -169,12 +203,10 @@ class ServerProcess(QtCore.QObject):
         self.output.emit(data)
 
 class ServerProcessPool(QtCore.QObject):
-    default_base_port = 23000
-
+    """Keeps track of running ServerProcesses and the listen ports they use."""
     def __init__(self, parent=None):
         super(ServerProcessPool, self).__init__(parent)
         self.log                = util.make_logging_source_adapter(__name__, self)
-        self.base_port          = ServerProcessPool.default_base_port
         self._procs_by_port     = weakref.WeakValueDictionary()
         self._unavailable_ports = set()
 
@@ -194,7 +226,7 @@ class ServerProcessPool(QtCore.QObject):
         in_use = set(self._procs_by_port.keys())
         in_use = in_use.union(self._unavailable_ports)
 
-        for p in xrange(self.base_port, 65535):
+        for p in xrange(BASE_PORT, 65535):
             if p not in in_use:
                 return p
 
@@ -210,3 +242,32 @@ class ServerProcessPool(QtCore.QObject):
             process.start()
 
 pool = ServerProcessPool()
+
+if __name__ == "__main__":
+    import logging
+    import signal
+    import sys
+
+    logging.basicConfig(level=logging.DEBUG,
+            format='[%(asctime)-15s] [%(name)s.%(levelname)s] %(message)s')
+
+    app = QtCore.QCoreApplication([])
+
+    def signal_handler(signum, frame):
+        app.quit()
+    signal.signal(signal.SIGINT, signal_handler)
+
+    sp1 = ServerProcess(serial_port='/dev/ttyUSB0')
+    sp2 = ServerProcess(serial_port='/dev/ttyUSB0')
+    sp3 = ServerProcess(serial_port='/dev/ttyUSB0', listen_address="foobar")
+    sp4 = ServerProcess(serial_port='/dev/ttyUSB0', listen_address="1.2.3.4")
+
+    def sp_start_done(f):
+        print "start() done:", f.result()
+
+    sp1.start().add_done_callback(sp_start_done)
+    sp2.start().add_done_callback(sp_start_done)
+    sp3.start().add_done_callback(sp_start_done)
+    sp4.start()
+
+    sys.exit(app.exec_())
