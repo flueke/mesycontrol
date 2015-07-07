0e67ac1ed679697b50000d28247429ed11b4c79c
Florian Lüke <florianlueke@gmx.net>
2015-07-07 10:43:19 +0200

    wip: config util & gui

--- src/client/mesycontrol/config_util.py
+++ src/client/mesycontrol/config_util.py
@@ -1,89 +1,7 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# Author: Florian Lüke <florianlueke@gmx.net>
-
-from qt import pyqtSlot
-from qt import Qt
-from qt import QtCore
-
-import future
-import hardware_controller
-import itertools
-import model_util
-import util
-import sys
-
-class GeneratorRunner(QtCore.QObject):
-    def __init__(self, generator=None, parent=None):
-        super(GeneratorRunner, self).__init__(parent)
-
-        self.generator = generator
-        self.log = util.make_logging_source_adapter(__name__, self)
-
-    # Note about using QMetaObject.invokeMethod() in start() and
-    # _future_yielded():
-    #
-    # If the generator yields a Future object that completes immediately a
-    # callback added via add_done_callback() will also be executed immediately.
-    # As the on_done() callback in _future_yielded() calls _next() again the
-    # call stack grows. This can quickly lead to the call stack exceeding its
-    # maximum size.
-    #
-    # To avoid this the call to _next() is queued in the Qt event loop and the
-    # current invocation of _next() can return.
-
-    def start(self):
-        """Start execution of the generator.
-        Requires a running Qt event loop (or calls to processEvents()).
-        Returns a Future that fullfills on generator termination.
+        """Handles the case where the generator yields a Future object.
+        The default action is to wait for the future to complete and then send
+        the future back into the generator.
         """
-        self._start()
-
-        if self.generator is None:
-            raise RuntimeError("No generator function set")
-
-        self.arg    = None
-        self.result = future.Future()
-
-        QtCore.QMetaObject.invokeMethod(self, "_next", Qt.QueuedConnection)
-
-        return self.result
-
-    @pyqtSlot()
-    def _next(self):
-        while True:
-            try:
-                obj = self.generator.send(self.arg)
-
-                if isinstance(obj, future.Future):
-                    if self._future_yielded(obj):
-                        return
-
-                elif isinstance(obj, ProgressUpdate):
-                    self._progress_update(obj)
-
-                else:
-                    self.arg, do_return = self._object_yielded(obj)
-                    if do_return:
-                        return
-
-            except StopIteration:
-                self._stop_iteration()
-                return
-
-            except Exception as e:
-                try:
-                    self._exception(e)
-                except Exception as e:
-                    self.result.set_exception(e)
-                    self.generator.close()
-                    self.log.exception("Generator")
-                    return
-
-    def _start(self):
-        pass
-
-    def _future_yielded(self, f):
         def on_done(f):
             self.arg = f
             QtCore.QMetaObject.invokeMethod(self, "_next", Qt.QueuedConnection)
@@ -91,19 +9,27 @@ class GeneratorRunner(QtCore.QObject):
         f.add_done_callback(on_done)
         return True
 
-    def _object_yielded(self, obj):
-        raise NotImplementedError()
-
     def _progress_update(self, progress):
         self.result.set_progress_range(0, progress.total)
         self.result.set_progress(progress.current)
         self.result.set_progress_text(progress.text)
 
+    def _object_yielded(self, obj):
+        """Called if the generator yields any object other than Future or
+        ProgressUpdate."""
+        raise NotImplementedError()
+
     def _stop_iteration(self):
+        """Called on encountering a StopIteration exception. The default is to
+        set the result futures value to True."""
         if not self.result.done():
             self.result.set_result(True)
 
     def _exception(self, e):
+        """Called on encountering an exception other than StopIteration.  If
+        the exception is an instance of Aborted the result futures value will
+        be set to False. Otherwise the exception is reraised with the original
+        traceback."""
         if isinstance(e, Aborted):
             self.result.set_result(False)
         else:
@@ -146,11 +72,10 @@ class MissingDestinationMRC(RuntimeError):
     def __init__(self, url):
         self.url = url
 
-ACTION_SKIP, ACTION_ABORT, ACTION_RETRY = range(3)
-
 class Aborted(RuntimeError):
     pass
 
+ACTION_SKIP, ACTION_ABORT, ACTION_RETRY = range(3)
 
 def apply_device_config(source, dest, device_profile):
     """Apply device config operation in the form of a generator.
@@ -282,95 +207,6 @@ def apply_device_config(source, dest, device_profile):
     finally:
         pass
 
-def apply_setup(source, dest, device_registry):
-    """
-    source: a basic_model.MRCRegistry compatible object; usually a
-            config_model.Setup instance.
-    dest  : a basic_model.MRCRegistry compatible object; usually the root of
-            the hardware tree.
-    device_registry: a DeviceRegistry compatible object providing DeviceProfile
-        instances by idc.
-    """
-    
-    # * Compare structure of source and dest. Report
-    #   missing/disconnected/silenced MRCs.
-    #   Ignore additional MRCs in dest (XXX: or yield a warning?)
-    #   Answers & reactions:
-    #     missing: skip, abort, try again
-    #     disconnected: skip, abort, try again
-    #     silenced: skip, abort, try again
-    #   - Compare the structure of the matching MRCs:
-    #     Report missing devices, address conflicts and idc conflicts.
-    #     Answers & reactions:
-    #       missing: skip device, skip mrc, abort, try again
-    #       addr conflict: skip bus, skip mrc, abort, try again
-    #       idc conflict: skip device, skip mrc, abort, try again
-
-    for src_mrc in source:
-        dest_mrc = dest.get_mrc(src_mrc.url)
-
-        if dest_mrc is None:
-            action = yield MissingDestinationMRC(url=src_mrc.url)
-
-            if action == ACTION_SKIP:
-                continue
-
-            if action == ACTION_ABORT:
-                raise Aborted()
-
-        for src_dev in src_mrc:
-            dest_dev     = dest_mrc.get_device(src_dev.bus, src_dev.address)
-
-            if dest_dev is None:
-                action = ACTION_RETRY
-
-                while action == ACTION_RETRY:
-                    dest_dev = dest_mrc.get_device(src_dev.bus, src_dev.address)
-
-                    if dest_dev is not None:
-                        break
-
-                    action = yield MissingDestinationDevice(
-                            url=src_mrc.url, bus=src_dev.bus, dev=src_dev.address)
-
-                    if action == ACTION_SKIP:
-                        break
-
-                    if action == ACTION_ABORT:
-                        raise Aborted()
-
-                if action == ACTION_SKIP:
-                    continue
-
-            profile      = device_registry.get_profile(src_dev.idc)
-            apply_config = apply_device_config(src_dev, dest_dev, profile)
-            arg          = None
-
-            while True:
-                try:
-                    obj = apply_config.send(arg)
-                    arg = yield obj
-                except IDCConflict as e:
-                    action = yield e
-
-                    if action == ACTION_SKIP:
-                        break
-
-                    if action == ACTION_ABORT:
-                        raise Aborted()
-
-                    if action == ACTION_RETRY:
-                        continue # FIXME: need to retry with a new apply_device_config generator!
-
-                except StopIteration:
-                    break
-                except GeneratorExit:
-                    apply_config.close()
-                    return
-
-            if action == ACTION_SKIP:
-                continue
-
 def establish_connections(setup, hardware_registry):
     for cfg_mrc in setup:
         hw_mrc = hardware_registry.get_mrc(cfg_mrc.url)
@@ -438,3 +274,100 @@ def connect_and_apply_setup(setup, hw_registry, device_registry):
         except GeneratorExit:
             gen.close()
             return
+
+def apply_setup(source, dest, device_registry):
+
+    def _apply_device_config(src_dev, dest_mrc):
+        action = ACTION_RETRY
+        dest_dev = None
+
+        while action == ACTION_RETRY:
+            dest_dev = dest_mrc.get_device(src_dev.bus, src_dev.address)
+
+            if dest_dev is not None:
+                break
+
+            action = yield MissingDestinationDevice(
+                    url=src_dev.mrc.url, bus=src_dev.bus, dev=src_dev.address)
+
+            if action == ACTION_SKIP:
+                raise StopIteration()
+
+            if action == ACTION_ABORT:
+                raise Aborted()
+
+        gen = apply_device_config(src_dev, dest_dev,
+                device_registry.get_profile(src_dev.idc))
+        arg = None
+
+        while True:
+            try:
+                obj = gen.send(arg)
+                arg = yield obj
+            except StopIteration:
+                break
+            except GeneratorExit:
+                gen.close()
+                return
+
+    def _apply_mrc_config(src_mrc):
+        action   = ACTION_RETRY
+        dest_mrc = None
+
+        while action == ACTION_RETRY:
+            dest_mrc = dest.get_mrc(src_mrc.url)
+
+            if dest_mrc is not None:
+                break
+
+            action = yield MissingDestinationMRC(url=src_mrc.url)
+
+            if action == ACTION_SKIP:
+                raise StopIteration()
+
+            if action == ACTION_ABORT:
+                raise Aborted()
+
+        if not dest_mrc.is_connected():
+            return
+
+        for src_dev in src_mrc:
+            action = ACTION_RETRY
+
+            while action == ACTION_RETRY:
+
+                gen = _apply_device_config(src_dev, dest_mrc)
+                arg = None
+
+                while True:
+                    try:
+                        obj = gen.send(arg)
+                        arg = yield obj
+                    except StopIteration:
+                        action = None
+                        break
+                    except GeneratorExit:
+                        gen.close()
+                        return
+                    except IDCConflict as e:
+                        action = yield e
+
+                        if action in (ACTION_SKIP, ACTION_RETRY):
+                            break
+
+                        if action == ACTION_ABORT:
+                            raise Aborted()
+
+    for src_mrc in source:
+        gen = _apply_mrc_config(src_mrc)
+        arg = None
+
+        while True:
+            try:
+                obj = gen.send(arg)
+                arg = yield obj
+            except StopIteration:
+                break
+            except GeneratorExit:
+                gen.close()
+                return
