dce0f42af249ccb5d1c0cec26c0df3955c260b19
Florian Lüke <florianlueke@gmx.net>
2015-07-14 14:19:26 +0200

    handle cancelled futures in set_parameter

--- src/client/mesycontrol/specialized_device.py
+++ src/client/mesycontrol/specialized_device.py
@@ -6,8 +6,8 @@ from qt import pyqtProperty
 from qt import pyqtSignal
 from qt import QtCore
 
-import basic_model as bm
 import future
+import util
 
 class DeviceBase(QtCore.QObject):
     """Acts as a decorator for an app_model.Device. Should be subclassed to
@@ -26,8 +26,8 @@ class DeviceBase(QtCore.QObject):
     def __init__(self, app_device, read_mode, write_mode, parent=None):
         """
         app_device: app_model.Device
-        read_mode:  bm.HARDWARE | bm.CONFIG
-        write_mode: bm.HARDWARE | bm.CONFIG | bm.COMBINED
+        read_mode:  util.HARDWARE | util.CONFIG
+        write_mode: util.HARDWARE | util.CONFIG | util.COMBINED
         """
 
         super(DeviceBase, self).__init__(parent)
@@ -59,46 +59,52 @@ class DeviceBase(QtCore.QObject):
             self._write_mode = mode
             self.write_mode_changed.emit(self.write_mode)
 
-    hw              = pyqtProperty(object, lambda s: s.app_device.get_hardware(), notify=hardware_set)
-    cfg             = pyqtProperty(object, lambda s: s.app_device.get_config(), notify=config_set)
-    mrc             = pyqtProperty(object, lambda s: s.app_device.get_mrc(), notify=mrc_changed)
-    idc_conflict    = pyqtProperty(bool, lambda s: s.app_device.has_idc_conflict(), notify=idc_conflict_changed)
-    profile         = pyqtProperty(object, lambda s: s.app_device.get_profile(), notify=profile_changed)
-    config_applied  = pyqtProperty(bool, lambda s: s.app_device.is_config_applied(), notify=config_applied_changed)
+    #hw              = pyqtProperty(object, lambda s: s.app_device.get_hardware(), notify=hardware_set)
+    #cfg             = pyqtProperty(object, lambda s: s.app_device.get_config(), notify=config_set)
+    #mrc             = pyqtProperty(object, lambda s: s.app_device.get_mrc(), notify=mrc_changed)
+    #idc_conflict    = pyqtProperty(bool, lambda s: s.app_device.has_idc_conflict(), notify=idc_conflict_changed)
+    #profile         = pyqtProperty(object, lambda s: s.app_device.get_profile(), notify=profile_changed)
+    #config_applied  = pyqtProperty(bool, lambda s: s.app_device.is_config_applied(), notify=config_applied_changed)
     read_mode       = pyqtProperty(object, get_read_mode, set_read_mode, notify=read_mode_changed)
     write_mode      = pyqtProperty(object, get_write_mode, set_write_mode, notify=write_mode_changed)
 
     def get_parameter(self, address_or_name):
         address = self.profile[address_or_name].address
-        dev = self.hw if self.read_mode == bm.HARDWARE else self.cfg
+        dev = self.hw if self.read_mode == util.HARDWARE else self.cfg
         return dev.get_parameter(address)
 
     def set_parameter(self, address_or_name, value):
         address = self.profile[address_or_name].address
 
-        if self.write_mode == bm.COMBINED:
+        if self.write_mode == util.COMBINED:
 
             ret = future.Future()
 
             def on_hw_write_done(f):
-                if f.exception() is not None:
-                    ret.set_exception(f.exception())
-                else:
-                    ret.set_result(f.result())
+                try:
+                    if f.exception() is not None:
+                        ret.set_exception(f.exception())
+                    else:
+                        ret.set_result(f.result())
+                except future.CancelledError:
+                    pass
 
             def on_cfg_write_done(f):
-                if f.exception() is not None:
-                    ret.set_exception(f.exception())
-                else:
-                    self.hw.set_parameter(address, value
-                            ).add_done_callback(on_hw_write_done)
+                try:
+                    if f.exception() is not None:
+                        ret.set_exception(f.exception())
+                    else:
+                        self.hw.set_parameter(address, value
+                                ).add_done_callback(on_hw_write_done)
+                except future.CancelledError:
+                    pass
 
             self.cfg.set_parameter(address, value
                     ).add_done_callback(on_cfg_write_done)
 
             return ret
 
-        dev = self.hw if self.write_mode == bm.HARDWARE else self.cfg
+        dev = self.hw if self.write_mode == util.HARDWARE else self.cfg
         return dev.set_parameter(address, value)
 
     def __getattr__(self, attr):
