86269f0450960f2d65cf1d2adddcff54928047ac
Florian Lüke <florianlueke@gmx.net>
2015-08-06 16:55:48 +0200

    change: open_last_setup_at_start now defaults to True

--- src/client/mesycontrol_gui.py
+++ src/client/mesycontrol_gui.py
@@ -110,7 +110,7 @@ if __name__ == "__main__":
 
     if opts is not None and opts.setup is not None:
         setup_file = opts.setup
-    elif settings.value('Options/open_last_setup_at_start', False).toBool():
+    elif settings.value('Options/open_last_setup_at_start', True).toBool():
         setup_file = str(settings.value('Files/last_setup_file', str()).toString())
 
     with app_context.use(context):
