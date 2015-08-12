2ba005d6bc7265e9a748df3d2d7452f0220cabd0
Florian Lüke <florianlueke@gmx.net>
2015-08-12 10:53:52 +0200

    mhv4: updated param ranges and defaults

--- src/client/mesycontrol/devices/mhv4_profile.py
+++ src/client/mesycontrol/devices/mhv4_profile.py
@@ -2,6 +2,17 @@
 # -*- coding: utf-8 -*-
 # Author: Florian Lüke <florianlueke@gmx.net>
 
+class Polarity(object):
+    negative = 0
+    positive = 1
+
+    @staticmethod
+    def switch(pol):
+        if pol == Polarity.positive:
+            return Polarity.negative
+        return Polarity.positive
+
+
 voltage_10  = { 'label': 'V',       'name': 'volt',             'factor': 10.0 }
 voltage_80  = { 'label': 'V',       'name': 'volt_prec',        'factor': 80.0 }
 tcomp_slope = { 'label': 'V/°C',    'name': 'volt_per_deg',     'factor': 1000.0, 'offset': -10.0 }
@@ -15,10 +26,10 @@ profile_dict = {
         'idc': idc,
         'parameters': [
             # Enable. Mark channel enable setting as critical. 
-            { 'address': 4,   'name': 'channel0_enable_write', 'index': 0, 'critical': True },
-            { 'address': 5,   'name': 'channel1_enable_write', 'index': 1, 'critical': True },
-            { 'address': 6,   'name': 'channel2_enable_write', 'index': 2, 'critical': True },
-            { 'address': 7,   'name': 'channel3_enable_write', 'index': 3, 'critical': True },
+            { 'address': 4,   'name': 'channel0_enable_write', 'index': 0, 'critical': True, 'default': 0 },
+            { 'address': 5,   'name': 'channel1_enable_write', 'index': 1, 'critical': True, 'default': 0 },
+            { 'address': 6,   'name': 'channel2_enable_write', 'index': 2, 'critical': True, 'default': 0 },
+            { 'address': 7,   'name': 'channel3_enable_write', 'index': 3, 'critical': True, 'default': 0 },
 
             { 'address': 36,  'name': 'channel0_enable_read', 'index': 0, 'read_only': True, 'poll': True },
             { 'address': 37,  'name': 'channel1_enable_read', 'index': 1, 'read_only': True, 'poll': True },
@@ -26,10 +37,10 @@ profile_dict = {
             { 'address': 39,  'name': 'channel3_enable_read', 'index': 3, 'read_only': True, 'poll': True },
 
             # Polarity
-            { 'address': 14,  'name': 'channel0_polarity_write', 'index': 0, },
-            { 'address': 15,  'name': 'channel1_polarity_write', 'index': 1, },
-            { 'address': 16,  'name': 'channel2_polarity_write', 'index': 2, },
-            { 'address': 17,  'name': 'channel3_polarity_write', 'index': 3, },
+            { 'address': 14,  'name': 'channel0_polarity_write', 'index': 0, 'default': Polarity.positive},
+            { 'address': 15,  'name': 'channel1_polarity_write', 'index': 1, 'default': Polarity.positive},
+            { 'address': 16,  'name': 'channel2_polarity_write', 'index': 2, 'default': Polarity.positive},
+            { 'address': 17,  'name': 'channel3_polarity_write', 'index': 3, 'default': Polarity.positive},
 
             { 'address': 46,  'name': 'channel0_polarity_read', 'index': 0, 'read_only': True, 'poll': True },
             { 'address': 47,  'name': 'channel1_polarity_read', 'index': 1, 'read_only': True, 'poll': True },
@@ -48,10 +59,10 @@ profile_dict = {
             { 'address': 25,  'name': 'channel3_voltage_limit_read', 'index': 3, 'read_only': True, 'poll': True, 'units': [voltage_10] },
 
             # Current warning limits
-            { 'address':  8,  'name': 'channel0_current_limit_write', 'index': 0, 'units': [microamps] },
-            { 'address':  9,  'name': 'channel1_current_limit_write', 'index': 1, 'units': [microamps] },
-            { 'address': 10,  'name': 'channel2_current_limit_write', 'index': 2, 'units': [microamps] },
-            { 'address': 11,  'name': 'channel3_current_limit_write', 'index': 3, 'units': [microamps] },
+            { 'address':  8,  'name': 'channel0_current_limit_write', 'index': 0, 'range': (0, 20000), 'units': [microamps], 'default': 20000 },
+            { 'address':  9,  'name': 'channel1_current_limit_write', 'index': 1, 'range': (0, 20000), 'units': [microamps], 'default': 20000 },
+            { 'address': 10,  'name': 'channel2_current_limit_write', 'index': 2, 'range': (0, 20000), 'units': [microamps], 'default': 20000 },
+            { 'address': 11,  'name': 'channel3_current_limit_write', 'index': 3, 'range': (0, 20000), 'units': [microamps], 'default': 20000 },
 
             { 'address': 40,  'name': 'channel0_current_limit_read', 'index': 0, 'read_only': True, 'poll': True, 'units': [microamps] },
             { 'address': 41,  'name': 'channel1_current_limit_read', 'index': 1, 'read_only': True, 'poll': True, 'units': [microamps] },
@@ -71,10 +82,10 @@ profile_dict = {
             { 'address': 29,  'name': 'sensor3_temp_read', 'index': 3, 'read_only': True, 'poll': True, 'units': [celcius_10] },
 
             # Temperature compensation slope
-            { 'address': 64,  'name': 'channel0_tcomp_slope_write', 'index': 0, 'range': (0, 19999), 'units': [tcomp_slope] },
-            { 'address': 65,  'name': 'channel1_tcomp_slope_write', 'index': 1, 'range': (0, 19999), 'units': [tcomp_slope] },
-            { 'address': 66,  'name': 'channel2_tcomp_slope_write', 'index': 2, 'range': (0, 19999), 'units': [tcomp_slope] },
-            { 'address': 67,  'name': 'channel3_tcomp_slope_write', 'index': 3, 'range': (0, 19999), 'units': [tcomp_slope] },
+            { 'address': 64,  'name': 'channel0_tcomp_slope_write', 'index': 0, 'range': (0, 19999), 'units': [tcomp_slope], 'default': 10000 },
+            { 'address': 65,  'name': 'channel1_tcomp_slope_write', 'index': 1, 'range': (0, 19999), 'units': [tcomp_slope], 'default': 10000 },
+            { 'address': 66,  'name': 'channel2_tcomp_slope_write', 'index': 2, 'range': (0, 19999), 'units': [tcomp_slope], 'default': 10000 },
+            { 'address': 67,  'name': 'channel3_tcomp_slope_write', 'index': 3, 'range': (0, 19999), 'units': [tcomp_slope], 'default': 10000 },
 
             { 'address': 100,  'name': 'channel0_tcomp_slope_read', 'index': 0, 'read_only': True, 'poll': True, 'units': [tcomp_slope] },
             { 'address': 101,  'name': 'channel1_tcomp_slope_read', 'index': 1, 'read_only': True, 'poll': True, 'units': [tcomp_slope] },
@@ -82,10 +93,10 @@ profile_dict = {
             { 'address': 103,  'name': 'channel3_tcomp_slope_read', 'index': 3, 'read_only': True, 'poll': True, 'units': [tcomp_slope] },
 
             # Temperature compensation offset
-            { 'address': 68,  'name': 'channel0_tcomp_offset_write', 'index': 0, 'range': (0, 500), 'units': [celcius_10] },
-            { 'address': 69,  'name': 'channel1_tcomp_offset_write', 'index': 1, 'range': (0, 500), 'units': [celcius_10] },
-            { 'address': 70,  'name': 'channel2_tcomp_offset_write', 'index': 2, 'range': (0, 500), 'units': [celcius_10] },
-            { 'address': 71,  'name': 'channel3_tcomp_offset_write', 'index': 3, 'range': (0, 500), 'units': [celcius_10] },
+            { 'address': 68,  'name': 'channel0_tcomp_offset_write', 'index': 0, 'range': (0, 500), 'units': [celcius_10], 'default': 0 },
+            { 'address': 69,  'name': 'channel1_tcomp_offset_write', 'index': 1, 'range': (0, 500), 'units': [celcius_10], 'default': 0 },
+            { 'address': 70,  'name': 'channel2_tcomp_offset_write', 'index': 2, 'range': (0, 500), 'units': [celcius_10], 'default': 0 },
+            { 'address': 71,  'name': 'channel3_tcomp_offset_write', 'index': 3, 'range': (0, 500), 'units': [celcius_10], 'default': 0 },
 
             { 'address': 104,  'name': 'channel0_tcomp_offset_read', 'index': 0, 'read_only': True, 'poll': True, 'units': [celcius_10] },
             { 'address': 105,  'name': 'channel1_tcomp_offset_read', 'index': 1, 'read_only': True, 'poll': True, 'units': [celcius_10] },
@@ -93,10 +104,10 @@ profile_dict = {
             { 'address': 107,  'name': 'channel3_tcomp_offset_read', 'index': 3, 'read_only': True, 'poll': True, 'units': [celcius_10] },
 
             # Temperature compensation source
-            { 'address': 72,  'name': 'channel0_tcomp_source_write', 'index': 0, 'range': (0, 4)},
-            { 'address': 73,  'name': 'channel1_tcomp_source_write', 'index': 1, 'range': (0, 4)},
-            { 'address': 74,  'name': 'channel2_tcomp_source_write', 'index': 2, 'range': (0, 4)},
-            { 'address': 75,  'name': 'channel3_tcomp_source_write', 'index': 3, 'range': (0, 4)},
+            { 'address': 72,  'name': 'channel0_tcomp_source_write', 'index': 0, 'range': (0, 4), 'default': 4},
+            { 'address': 73,  'name': 'channel1_tcomp_source_write', 'index': 1, 'range': (0, 4), 'default': 4},
+            { 'address': 74,  'name': 'channel2_tcomp_source_write', 'index': 2, 'range': (0, 4), 'default': 4},
+            { 'address': 75,  'name': 'channel3_tcomp_source_write', 'index': 3, 'range': (0, 4), 'default': 4},
 
             { 'address': 108,  'name': 'channel0_tcomp_source_read', 'index': 0, 'read_only': True, 'poll': True},
             { 'address': 109,  'name': 'channel1_tcomp_source_read', 'index': 1, 'read_only': True, 'poll': True},
@@ -117,16 +128,16 @@ profile_dict = {
 
             # Misc
             { 'address': 44,  'name': 'rc_enable', 'read_only': True },
-            { 'address': 80,  'name': 'ramp_speed_write', 'range': (0, 3) },
+            { 'address': 80,  'name': 'ramp_speed_write', 'range': (0, 3), 'default': 0 },
             { 'address': 116, 'name': 'ramp_speed_read', 'read_only': True, 'poll': True },
 
             # Voltage
             # Keep at the end of the parameter list to make sure these
             # addresses are written last when loading a config.
-            { 'address': 0,   'name': 'channel0_voltage_write', 'index': 0, 'range': (0, 8000), 'units': [voltage_10] },
-            { 'address': 1,   'name': 'channel1_voltage_write', 'index': 1, 'range': (0, 8000), 'units': [voltage_10] },
-            { 'address': 2,   'name': 'channel2_voltage_write', 'index': 2, 'range': (0, 8000), 'units': [voltage_10] },
-            { 'address': 3,   'name': 'channel3_voltage_write', 'index': 3, 'range': (0, 8000), 'units': [voltage_10] },
+            { 'address': 0,   'name': 'channel0_voltage_write', 'index': 0, 'range': (0, 8000), 'units': [voltage_10], 'default': 0 },
+            { 'address': 1,   'name': 'channel1_voltage_write', 'index': 1, 'range': (0, 8000), 'units': [voltage_10], 'default': 0 },
+            { 'address': 2,   'name': 'channel2_voltage_write', 'index': 2, 'range': (0, 8000), 'units': [voltage_10], 'default': 0 },
+            { 'address': 3,   'name': 'channel3_voltage_write', 'index': 3, 'range': (0, 8000), 'units': [voltage_10], 'default': 0 },
 
             { 'address': 32,  'name': 'channel0_voltage_read', 'index': 0, 'read_only': True, 'poll': True, 'units': [voltage_10] },
             { 'address': 33,  'name': 'channel1_voltage_read', 'index': 1, 'read_only': True, 'poll': True, 'units': [voltage_10] },
