b6603ba141ade572dce4f051e3ccd2713ba2191b
Florian Lüke <florianlueke@gmx.net>
2015-08-13 12:06:26 +0200

    added About Dialog

--- src/client/mesycontrol/gui_mainwindow.py
+++ src/client/mesycontrol/gui_mainwindow.py
@@ -79,11 +79,81 @@ class MainWindow(QtGui.QMainWindow):
 
     @pyqtSlot()
     def on_actionAbout_triggered(self):
-        # TODO: add icon, text and version number
-        dialog = QtGui.QMessageBox(self)
-        dialog.setWindowTitle("About mesycontrol")
-        dialog.setStandardButtons(QtGui.QMessageBox.Ok)
-        dialog.exec_()
+        try:
+            from . import mc_version
+            version = mc_version.version
+        except ImportError:
+            version = "devel version"
+
+        d = QtGui.QDialog(self)
+        d.setWindowTitle("About mesycontrol")
+
+        license = QtGui.QTextBrowser(parent=d)
+        license.setWindowFlags(Qt.Window)
+        license.setWindowTitle("mesycontrol license")
+        license.setText("")
+
+        try:
+            f = QtCore.QFile(":/gpl.txt")
+            if not f.open(QtCore.QIODevice.ReadOnly | QtCore.QIODevice.Text):
+                return
+            license.setText(QtCore.QString(f.readAll()))
+        finally:
+            f.close()
+
+        l = QtGui.QVBoxLayout(d)
+
+        logo = QtGui.QLabel()
+        logo.setPixmap(QtGui.QPixmap(":/mesytec-logo.png"
+            ).scaledToWidth(300, Qt.SmoothTransformation))
+        l.addWidget(logo)
+
+        t = "mesycontrol - %s" % version
+        label = QtGui.QLabel(t)
+        font  = label.font()
+        font.setPointSize(15)
+        font.setBold(True)
+        label.setFont(font)
+        l.addWidget(label)
+
+        l.addWidget(QtGui.QLabel("Remote control for mesytec devices."))
+        l.addWidget(QtGui.QLabel(QtCore.QString.fromUtf8("© 2014-2015 mesytec GmbH & Co. KG")))
+
+        t = '<a href="mailto:info@mesytec.com">info@mesytec.com</a> - <a href="http://www.mesytec.com">www.mesytec.com</a>'
+        label = QtGui.QLabel(t)
+        label.setOpenExternalLinks(True)
+        l.addWidget(label)
+
+        l.addSpacing(20)
+
+        bl = QtGui.QHBoxLayout()
+
+        def license_button_clicked():
+            sz = license.size()
+            sz = sz.expandedTo(QtCore.QSize(500, 300))
+            license.resize(sz)
+            license.show()
+            license.raise_()
+
+        b = QtGui.QPushButton("&License", clicked=license_button_clicked)
+        bl.addWidget(b)
+
+        b = QtGui.QPushButton("&Close", clicked=d.close)
+        b.setAutoDefault(True)
+        b.setDefault(True)
+        bl.addWidget(b)
+
+        l.addLayout(bl)
+
+        for item in (l.itemAt(i) for i in range(l.count())):
+            item.setAlignment(Qt.AlignHCenter)
+
+            w = item.widget()
+
+            if isinstance(w, QtGui.QLabel):
+                w.setTextInteractionFlags(Qt.TextBrowserInteraction)
+
+        d.exec_()
 
     @pyqtSlot()
     def on_actionAbout_Qt_triggered(self):
