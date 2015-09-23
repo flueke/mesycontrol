63a237a217736518d8bbb53347db9d40535c79e8
Florian Lüke <florianlueke@gmx.net>
2015-09-23 15:51:37 +0200

    device_view: removed out-of-date comments

--- src/client/mesycontrol/device_view.py
+++ src/client/mesycontrol/device_view.py
@@ -6,12 +6,6 @@ from PyQt4 import QtGui
 
 import device_tableview
 
-# Add a hackish way to fetch device memory before device widget display here:
-# - initiate a fetch of missing device memory values
-# - display a progress bar in the center tracking the progress of the fetch
-# - once the fetch is done: hide the progress bar and instantiate and show the
-#   real widget_class
-
 class DeviceView(QtGui.QWidget):
     """Widget creating and showing either a specific device widget if one is
     available or the generic device table view.
