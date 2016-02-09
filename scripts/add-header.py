e630389d40d9111dd1bd3b5e0715fe7536b85aad
Florian Lüke <florianlueke@gmx.net>
2016-02-09 11:11:28 +0100

    wip: py file header

--- scripts/add-header.py
+++ scripts/add-header.py
@@ -3,6 +3,7 @@
 # Author: Florian Lüke <florianlueke@gmx.net>
 
 # add_header.py -i py-file-header.txt <python files>
+# find . -name '*.py' | xargs python ../../scripts/add-header.py -i ../../scripts/py-file-header.txt
 
 import argparse
 import fileinput
@@ -13,6 +14,21 @@ if __name__ == "__main__":
     parser.add_argument('source_files', nargs=argparse.REMAINDER)
 
     args = parser.parse_args()
+    header_lines = [l for l in args.input]
+    new_header_written = False
 
     for line in fileinput.input(args.source_files, inplace=True):
-        print line
+        if fileinput.isfirstline():
+            new_header_written = False
+
+        if (line.startswith('#!')
+                or line.startswith('# -*- coding')
+                or line.startswith('# Author')):
+            continue
+
+        if not new_header_written:
+            for header_line in header_lines:
+                print header_line,
+            new_header_written = True
+
+        print line,
