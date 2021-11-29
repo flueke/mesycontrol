84c8ca69b6273d92f93e32ce91723758d42dfde0
Florian Lüke <f.lueke@mesytec.com>
2021-11-29 10:41:48 +0100

    fix cx_freeze init script

--- src/client/cxfreeze_init.py
+++ src/client/cxfreeze_init.py
@@ -64,7 +64,7 @@ importer = zipimport.zipimporter(INITSCRIPT_ZIP_FILE_NAME)
 name, ext = os.path.splitext(os.path.basename(os.path.normcase(FILE_NAME)))
 moduleName = "%s__main__" % name
 code = importer.get_code(moduleName)
-exec code in m.__dict__
+exec(code, m.__dict__)
 
 versionInfo = sys.version_info[:3]
 if versionInfo >= (2, 5, 0) and versionInfo <= (2, 6, 4):
