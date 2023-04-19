52185909f611b9d73a1e137258e31aa4d6cbf4b0
Florian Lüke <f.lueke@mesytec.com>
2023-04-19 16:00:01 +0200

    mini WIP commit

--- mc_script_test.py
+++ mc_script_test.py
@@ -1,7 +1,16 @@
-from mesycontrol.script import get_script_context
+#from mesycontrol.script import get_script_context
+#
+#with get_script_context() as ctx:
+#    mrc = ctx.make_mrc("mc://localhost:23000")
+#    print(mrc)
+#    mrc.connectMrc()
+#    print(mrc)
 
-with get_script_context() as ctx:
-    mrc = ctx.make_mrc("mc://localhost:23000")
-    print(mrc)
-    mrc.connectMrc()
-    print(mrc)
+from mesycontrol import script as mcs
+
+# Direct server connection: "mc://localhost:23000"
+# Have to spawn a server process: "/dev/ttyUSB0"
+
+with mcs.make_mrc("/dev/ttyUSB0") as mrc:
+    bus0_result = mrc.scanbus(0)
+    bus1_result = mrc.scanbus(1)
