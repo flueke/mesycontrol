c23392cf2f883236e754ffca3d12105b16680543
Florian Lüke <f.lueke@mesytec.com>
2024-07-26 13:39:02 +0200

    toml gives working script entry point. almost everything works now!
    Let's see if pyinstaller is still able to create a frozen distribution...

---
+++ src/client/mesycontrol/scripts/__init__.py
@@ -0,0 +1,3 @@
+from .auto_poll_parameters import main as auto_poll_parameters_main
+from .scanbus_device_profiles import main as scanbus_device_profiles_main
+from .scanbus import main as scanbus_main
